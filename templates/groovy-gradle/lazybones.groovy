8ff630c75358b752d8179abf477cb0fc0eeb7bab
Christoph Frick <rid@ofnir.net>
2014-07-16 15:55:02 +0200

    groovy-groovy: check for scmExclusions availability

--- templates/groovy-gradle/lazybones.groovy
+++ templates/groovy-gradle/lazybones.groovy
@@ -1,3 +1,29 @@
 props = [:]
 processTemplates "build.gradle", props
-scmExclusions ".DS_Store", "*~", ".idea", "*.iml", "*.iws", "*.ipr", ".project", ".metadata", ".classpath", ".settings/", ".loadpath", ".gradle", ".gradletasknamecache", "/build", "/bin", "/tmp", "/out", "*.class", "*.tmp", "*.bak", "*.swp", "*~.nib", "*.jar", "*.war", "*.ear"
+if (hasFeature("scmExclusions")) {
+	scmExclusions ".DS_Store",
+		      "*~",
+		      ".idea",
+		      "*.iml",
+		      "*.iws",
+		      "*.ipr",
+		      ".project",
+		      ".metadata",
+		      ".classpath",
+		      ".settings/",
+		      ".loadpath",
+		      ".gradle",
+		      ".gradletasknamecache",
+		      "/build",
+		      "/bin",
+		      "/tmp",
+		      "/out",
+		      "*.class",
+		      "*.tmp",
+		      "*.bak",
+		      "*.swp",
+		      "*~.nib",
+		      "*.jar",
+		      "*.war",
+		      "*.ear"
+}
